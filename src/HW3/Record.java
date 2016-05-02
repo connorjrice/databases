3133dc8268eabe7189d020fb471ae4a385770fae
Connor Rice <connor.rice@coloradocollege.edu>
2016-05-01 20:53:01 -0600

    Narrowing in on findRecord

--- src/HW3/Record.java
+++ src/HW3/Record.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -13,16 +14,15 @@ import java.util.logging.Logger;
  */
 public class Record {
     
-    
-    public final byte TAB_BEG = 33;
+    // These are all used for enclosing data
+    public final byte TAB_BEG = 33;  
     public final byte TAB_END = 35;
-    public final byte TKEY_BEG = 40;
+    public final byte TKEY_BEG = 40; // Table key
     public final byte TKEY_END = 41;
     public final byte REL_BEG = 2;
     public final byte REL_END = 3;
     
     public Record() {
-        
         try {
             Files.delete(Paths.get("test.db"));
         } catch (IOException ex) {
@@ -36,28 +36,36 @@ public class Record {
      * @param key: key to find table
      */
     public void createTable(Object table, String key) {
-        byte[] tabledata = table.toString().getBytes();
-        byte[] keydata = key.getBytes();
-        
         StringBuilder sb = new StringBuilder();
-        // Format is TKEY_BEG|key|TKEY_END
+        addKey(sb, key.getBytes());
+        addSchema(sb, table.toString().getBytes());
+        writeTable(sb);
+    }
+    
+    private void addKey(StringBuilder sb, byte[] keydata) {
         sb.append(TKEY_BEG);
         for (byte b : keydata) {
             sb.append(String.format("%02X", b));
         }
         sb.append(TKEY_END);
+    }
+    
+    private void addSchema(StringBuilder sb, byte[] tabledata) {
         sb.append(TAB_BEG);
         for (byte b : tabledata) {
             sb.append(String.format("%02X", b));
         }
         sb.append(TAB_END);
-        byte[] converted = sb.toString().getBytes();
+    }
+    
+    private void writeTable(StringBuilder sb) {
         try {
             try (RandomAccessFile raf = new RandomAccessFile("test.db", "rw")) {
                 raf.seek(raf.length());
-                for (byte b : converted) {
+                for (byte b : sb.toString().getBytes()) {
                     raf.writeByte(b);
                 }
+                raf.close();
             }
         } catch (IOException ex) {
             Logger.getLogger(Record.class.getName()).log(Level.SEVERE, null, ex);
@@ -78,6 +86,7 @@ public class Record {
                 for (byte b : converted) {
                     raf.writeByte(b);
                 }
+                raf.close();
             }
         } catch (IOException ex) {
             Logger.getLogger(Record.class.getName()).log(Level.SEVERE, null, ex);
@@ -89,54 +98,74 @@ public class Record {
     public void findRecord(String query, String key) {
         StringBuilder record = new StringBuilder();
         byte[] keydata = key.getBytes();
+        byte[] querydata = query.getBytes();
         try {
             RandomAccessFile raf = new RandomAccessFile("test.db", "rw");
-            byte[] querydata = query.getBytes();
             long length = raf.length()-1;
             long position = 0;
             boolean queryfound = false;
             while (position < length && !queryfound) {
                 position = findFirstTuple(raf, keydata);
                 raf.seek(position);
-                // At this point we are looking at the first relation in the table
+                byte[] relation = getRelation(raf);
                 
-                raf.seek(position);
-                byte b = raf.readByte();
-                if (b == TKEY_BEG) { // We have found
-                    // TODO: Check if key is correct
-                    while (b != TKEY_END) {
+                // At this point we are looking at the first relation
+                if (byteCompare(raf, query.getBytes())) { // If we find our query
+                    byte b = raf.readByte();
+                    ArrayList<Byte> a = new ArrayList<>();
+                    while (b != REL_END) {
+                        a.add(b);
                         b = raf.readByte();
-                        position++;
                     }
-                    if (b == TAB_BEG) { // Now we're into the table that we want
-                        b = raf.readByte();
-                        position += 1;
-                        while (b != TAB_END) {
-                            if (b == REL_BEG) {
-                                long start = raf.getFilePointer();
-                                if (checkRelation(raf, querydata, position, length)) {
-                                    queryfound = true;
-                                    raf.seek(start);
-                                    position = start;
-                                    while (b != TAB_END) {
-                                        b = raf.readByte();
-                                        position++;
-                                        record.append(Integer.toHexString(b));
-                                    }
-                                } else {
-                                    
-                                }
-                            }
-                        }
-                    }
+                } else { // Move on to next
+                    
                 }
+                raf.close();
             } 
         } catch (IOException ex) {
             Logger.getLogger(Record.class.getName()).log(Level.SEVERE, null, ex);
         }
         Logger.getLogger(Record.class.getName()).log(Level.INFO, record.toString());
-            
     }
+    
+    private boolean contains(byte[] a, byte[] b) {
+        boolean contains = false;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == b[0]) {
+                
+            }
+        }
+        return contains;
+    }
+    
+    
+    private byte[] getRelation(RandomAccessFile raf) throws IOException {
+        long position = raf.getFilePointer();
+        int relationLength = getRelationLength(raf);
+        byte[] relation = new byte[relationLength];
+        if (relation.length > 0) {
+            for (int i = 0; i < relationLength; i++) {
+                relation[i] = raf.readByte();
+            }
+        }
+        return relation;
+        
+    }
+    
+    private int getRelationLength(RandomAccessFile raf) throws IOException {
+        long position = raf.getFilePointer();
+        int count = 0;
+        raf.seek(position-1);
+        byte b = raf.readByte();
+        if (b == REL_BEG) {
+            while (b != REL_END) {
+                b = raf.readByte();
+                count++;
+            }
+        }
+        return count;
+    }
+    
     
     /**
      * Provide a long that is the offset indicating the location of the 
@@ -147,38 +176,59 @@ public class Record {
      * @param keydata
      * @return 
      */
-    private long findFirstTuple(RandomAccessFile raf, byte[] keydata) {
-        long position = -1l;
+    private long findFirstTuple(RandomAccessFile raf, byte[] keydata) throws IOException {
         try {
             //position = raf.getFilePointer();
             byte b = raf.readByte();
-            if (b == TKEY_BEG) { // If we are at the 
-                
-            } else {
-                b = raf.readByte();
+            if (b == TKEY_BEG) {
+                if (byteCompare(raf, keydata)) {
+                    while (b != TKEY_END) {
+                        b = raf.readByte();
+                    }
+                    if (b == TAB_BEG) {
+                        while (b != TAB_END) {
+                            b = raf.readByte();
+                        }
+                    }
+                    if (b == REL_BEG) {
+                        b = raf.readByte();
+                        return raf.getFilePointer();
+                    }
+                }
+            } else { // Find TKEY_BEG
+                while (b != TKEY_BEG && raf.getFilePointer() < raf.length()-1){
+                    b = raf.readByte();                    
+                }
             }
             
         } catch (IOException ex) {
             Logger.getLogger(Record.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return position;
+        return raf.getFilePointer();
     }
     
-    private boolean checkRelation(RandomAccessFile raf, byte[] query, long position, long length) {
+    /**
+     * Compares the current place of raf to the byte[] data
+     * @param raf
+     * @param data
+     * @return 
+     */
+    private boolean byteCompare(RandomAccessFile raf, byte[] data) {
         boolean isEqual = false;
         try {
+            long startingposition = raf.getFilePointer();
             isEqual = true;
             byte curbyte = raf.readByte();
-            for (int i = 0; i < query.length; i++) {
-                if (curbyte != query[i]) {
+            for (int i = 0; i < data.length; i++) {
+                if (curbyte != data[i]) {
                     isEqual = false;
                     break;
                 }
             }
+            raf.seek(startingposition);
         } catch (IOException ex) {
             Logger.getLogger(Record.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
         return isEqual;
     }
     
